4e1e4922344a91b573f5235e91e747f95e658055
Jen <jenny_t152@yahoo.co.uk>
2015-09-18 15:17:33 +0100

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:\\Users\\jenoma\\djangogirls\\blog\\template\\blog'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
